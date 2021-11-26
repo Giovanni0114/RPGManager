df3eb852814d48e8d0ff0c14453b8473f79469e0
Giovanni1414 <janko.juroszek@gmail.com>
2021-11-26 12:46:00 +0100

    Another happy commit

---
+++ arrow_navigation.js
@@ -0,0 +1,20 @@
+let a = document.getElementsByClassName('counting');
+let count = a[a.length - 1].innerHTML;
+
+
+
+
+document.onkeydown = function(event){
+	switch (event.keyCode){
+		case 37: //left
+			break;
+		case 38: //up
+			break;
+		case 39: //
+			break;
+		case 40: //left
+			break;
+		default:
+			break;
+	}
+}
