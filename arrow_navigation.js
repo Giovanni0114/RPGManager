7c7ed80e0d8e6805c07b20c6fea05a04481f4cc1
Giovanni1414 <janko.juroszek@gmail.com>
2021-11-30 15:11:11 +0100

    arrow navigation

--- arrow_navigation.js
+++ arrow_navigation.js
@@ -1,4 +1,6 @@
 let a = document.getElementsByClassName('counting');
+let b = document.getElementsByClassName('counting selected')[0].innerHTML;
+
 let count = a[a.length - 1].innerHTML;
 
 
@@ -7,13 +9,26 @@ let count = a[a.length - 1].innerHTML;
 document.onkeydown = function(event){
 	switch (event.keyCode){
 		case 37: //left
-			break;
-		case 38: //up
-			break;
-		case 39: //
-			break;
-		case 40: //left
-			break;
+			if(a != 1){
+				window.location.assign(window.location.pathname.split('/')[2]+"?id="+String(b*1-1))
+			}
+				break;
+				case 38: //up
+				if(a != b){
+					window.location.assign(window.location.pathname.split('/')[2]+"?id="+String(count))
+				}
+				break;
+				case 39: //right
+				if(a != 1){
+					window.location.assign(window.location.pathname.split('/')[2]+"?id="+String(b*1+1))
+				}
+				break;
+				case 40: //left
+				
+				if(a != 1){
+					window.location.assign(window.location.pathname.split('/')[2]+"?id=1")
+				}
+				break;
 		default:
 			break;
 	}
